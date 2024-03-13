d72d21a5bf41109942cd6701fc1229bdfc5358ba
mahrus-rohisyam <mahrusaim@gmail.comm>
2024-03-13 15:52:22 +0700

    Fixing Header, Footer and Building Query Post

---
+++ includes/front/enqueue.php
@@ -0,0 +1,14 @@
+<?php
+
+function u_enqueue()
+{
+  // Registration
+  wp_register_style('u_rubik_and_pacifico', 'https://fonts.googleapis.com/css2?family=Pacifico&family=Rubik:wght@300;400;500;700&display=swap', [], null);
+  wp_register_style('u_bootstrap_icon', get_theme_file_uri("assets/bootstrap-icons/bootstrap-icons.css"));
+  wp_register_style('u_theme', get_theme_file_uri('assets/public/index.css'));
+
+  // Queue
+  wp_enqueue_style('u_rubik_and_pacifico');
+  wp_enqueue_style('u_bootstrap_icon');
+  wp_enqueue_style('u_theme');
+};
